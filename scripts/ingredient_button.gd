6626df62e330b7e952ff03757976c083e8c8506b
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-25 18:35:20 +0200

    feat: add the ingredient button

---
+++ scripts/ingredient_button.gd
@@ -0,0 +1,22 @@
+extends CheckButton
+
+signal item_toggled
+
+var model: IngredientsData.IngredientModel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func add_ingredient(item):
+	model = item
+	self.text = IngredientsData.Ingredient.keys()[model.ingredient]
+	self.button_pressed = model.is_checked
+	
+func _on_toggled(toggled_on: bool) -> void:
+	model.is_checked = toggled_on
+	item_toggled.emit()
