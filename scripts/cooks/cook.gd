ef249081898d9bc8e58b2d537a7c951bb975d9ee
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-27 20:32:58 +0200

    feat: add cooker's assets and background

--- scripts/cooks/cook.gd
+++ scripts/cooks/cook.gd
@@ -1,9 +1,20 @@
-extends Button
+extends TextureButton
 
 @export var cook_data: CookData = null
 
 func _ready() -> void:
-	text = CookData.Cooks.keys()[cook_data.cook]
+	#text = CookData.Cooks.keys()[cook_data.cook]
+	self.texture_normal = cook_data.normal
+	self.texture_hover = cook_data.hightlight
+	
+	# Get the image from the texture normal
+	var image = texture_normal.get_image()
+	# Create the BitMap
+	var bitmap = BitMap.new()
+	# Fill it from the image alpha
+	bitmap.create_from_image_alpha(image)
+	# Assign it to the mask
+	texture_click_mask = bitmap
 
 func _on_button_up() -> void:
 	print("\n" + CookData.Cooks.keys()[cook_data.cook] + ": " + cook_data.statement)
--- scripts/cooks/cook_data.gd
+++ scripts/cooks/cook_data.gd
@@ -5,3 +5,5 @@ enum Cooks { ROSE, SIMON, PETER }
 
 @export var cook: Cooks = 0
 @export var statement: String = ""
+@export var normal: AtlasTexture
+@export var hightlight: AtlasTexture
