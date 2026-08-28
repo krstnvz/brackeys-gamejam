33e92cb8d94f6c503256a9874de3971c76b7226b
Kristýna Voženílková <99798404+krstnvz@users.noreply.github.com>
2026-08-28 21:02:35 +0200

    data: resources for level 2

---
+++ scripts/data scripts/poison_data.gd
@@ -0,0 +1,5 @@
+class_name PoisonData
+extends Resource
+
+@export var name: String = ""
+@export var lines: Array[String] = []
---
+++ scripts/data scripts/tipoff_data.gd
@@ -0,0 +1,4 @@
+class_name TipOffData
+extends Resource
+
+@export var lines: Array[String] = []
