beeb4cf64ada1d9b74a3365beb775a0130ad9745
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-28 23:19:55 +0200

    feat: add toggle mode to ingredient button

--- scripts/ingredients/ingredients_data.gd
+++ scripts/ingredients/ingredients_data.gd
@@ -34,29 +34,6 @@ enum Ingredient {
 	MAX_INGREDIENT
 }
 
-var allIngredients: Array[IngredientModel] = []
-
-func createAllIngredientsModel():
-	for ingredient in Ingredient.MAX_INGREDIENT:
-		var model = IngredientModel.new()
-		model.ingredient = ingredient
-		allIngredients.append(model)
-
-func crossAllIngredientsOut():
-	for ingredient in allIngredients:
-		ingredient.is_checked = false
-		
-func is_specific_ingredient(model, expected):
-	return model.ingredient == expected
-
-func crossIngredient(ingredient):
-	var index = allIngredients.find_custom(
-		is_specific_ingredient.bind(ingredient)
-	)
-	
-	if index >= 0:
-		allIngredients[index].is_checked = true
-
 class IngredientModel:
 	var ingredient: Ingredient
 	var is_checked: bool = false
@@ -125,3 +102,26 @@ class IngredientModel:
 				return "Blueberry"
 			_:
 				return "NONE"
+
+var allIngredients: Array[IngredientModel] = []
+
+func createAllIngredientsModel():
+	for ingredient in Ingredient.MAX_INGREDIENT:
+		var model = IngredientModel.new()
+		model.ingredient = ingredient
+		allIngredients.append(model)
+
+func crossAllIngredientsOut():
+	for ingredient in allIngredients:
+		ingredient.is_checked = false
+		
+func is_specific_ingredient(model, expected):
+	return model.ingredient == expected
+
+func crossIngredient(ingredient):
+	var index = allIngredients.find_custom(
+		is_specific_ingredient.bind(ingredient)
+	)
+	
+	if index >= 0:
+		allIngredients[index].is_checked = true
