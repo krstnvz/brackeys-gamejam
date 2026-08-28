852c33916d2a93372c04ab60deb8087ceeee27af
Kristýna Voženílková <99798404+krstnvz@users.noreply.github.com>
2026-08-28 23:37:23 +0200

    sprites: dishes & UI buttons

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -12,3 +12,9 @@ func _on_submit_button_button_up() -> void:
 
 func _on_button_button_up() -> void:
 	Navigation.go_to_menu()
+
+func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
