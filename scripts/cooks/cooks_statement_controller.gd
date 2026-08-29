ed743bab3b5a4d37cf64542f0324025c379d8305
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-29 20:08:59 +0200

    feat: control the statement visibility

--- scripts/cooks/cooks_statement_controller.gd
+++ scripts/cooks/cooks_statement_controller.gd
@@ -1,5 +1,9 @@
 extends Node
 
+@export var cooks: Array[CanvasLayer]
+
+var current_visible: CanvasLayer = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -7,3 +11,27 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_visibility_button_up() -> void:
+	pass
+
+func _on_statement_overlay_visibility_changed() -> void:
+	var is_all_hidden = true
+	for cook in cooks:
+		if cook.visible:
+			is_all_hidden = false
+			break
+	
+	if is_all_hidden:
+		current_visible = null
+	
+	if current_visible == null:
+		for cook in cooks:
+			if cook.visible:
+				current_visible = cook
+				break
+	else:
+		current_visible.visible = false
+		for cook in cooks:
+			if cook.visible:
+				current_visible = cook
