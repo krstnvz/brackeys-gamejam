4a602ae85e42c215a4dcfb5f94e203304c465f50
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-29 13:25:35 +0200

    feat: add statement dialog for the cooks

--- scripts/cooks/cook_statement.gd
+++ scripts/cooks/cook_statement.gd
@@ -1,10 +1,11 @@
-extends RichTextLabel
+extends PanelContainer
 
 @export var cook_data: CookData = null
+@onready var rich_text_label: RichTextLabel = $MarginContainer/RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = cook_data.statement
+	rich_text_label.text = cook_data.statement
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
