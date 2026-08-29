74c7f7936037748e0eb7463fadeed353c1b82565
Dyego de Jesus Silva <dyego.j.silva@gmail.com>
2026-08-29 22:11:41 +0200

    fix: tutorial visibility

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,3 +20,6 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_help_button_button_up() -> void:
+	canvas_layer.visible = true
