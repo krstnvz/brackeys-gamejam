d4fe4fcc3a5674c8fe81d59cc6be738113744f9c
Kristýna Voženílková <99798404+krstnvz@users.noreply.github.com>
2026-08-30 08:54:52 +0200

    fix: global audio state

--- scripts/mute_button.gd
+++ scripts/mute_button.gd
@@ -1,5 +1,9 @@
 extends TextureButton
 
+func _ready() -> void:
+	if GlobalAudio.is_paused():
+		button_pressed = true	
+
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		GlobalAudio.pause()
--- scripts/singletons/global_audio.gd
+++ scripts/singletons/global_audio.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 var audio_pos: float = 0.0
+var paused: bool = false
 
 func pause():
 	audio_pos = $AudioStreamPlayer2D.get_playback_position()
 	$AudioStreamPlayer2D.stop()
+	paused = true
 	
 func play():
 	$AudioStreamPlayer2D.play(audio_pos)
+	paused = false
+	
+func is_paused() -> bool:
+	return paused
